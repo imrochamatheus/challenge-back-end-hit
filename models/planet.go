d947dab4f66dda9c8eed0db244570231b9ea0433
Matheus Rocha <im.rochamatheus@gmail.com>
2024-03-14 19:48:19 -0300

    feat: define planet struct

--- models/planet.go
+++ models/planet.go
@@ -3,21 +3,19 @@ package models
 import "time"
 
 type Planet struct {
-	ID        string    `json:"id"`
-	Name      string    `json:"name" binding:"required"`
-	Ground    string    `json:"ground" binding:"required"`
-	Climate   string    `json:"climate" binding:"required"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at"`
+	ID          int64  	  `json:"id"`
+	Name        string    `json:"name" binding:"required"`
+	Ground      string    `json:"ground" binding:"required"`
+	Climate     string    `json:"climate" binding:"required"`
+	Appearances int64    `json:"appearances" binding:"required"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
-type PlanetResponse struct {
-	ID        string    `json:"id"`
-	Name      string    `json:"name" binding:"required"`
-	Ground    string    `json:"ground" binding:"required"`
-	Climate   string    `json:"climate" binding:"required"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at,omitempty"`
+type CreatePlanet struct {
+	ID          int64     `json:"id"`
+	Name        string    `json:"name" binding:"required"`
+	Ground      string    `json:"ground" binding:"required"`
+	Climate     string    `json:"climate" binding:"required"`
+	Appearances int64    `json:"appearances" binding:"required"`
 }
