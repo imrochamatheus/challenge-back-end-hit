4e8dd733ce1f5436b6c224f9b5462f55d2285a41
Matheus Rocha <im.rochamatheus@gmail.com>
2024-03-19 15:14:46 -0300

    feat: add util function to prepare queries

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,26 @@
 package utils
 
-import "os"
+import (
+	"database/sql"
+	"os"
+)
 
 func ReadQueryFile(path string) (string, error) {
 	data, err := os.ReadFile(path)
 
 	return string(data), err
+}
+
+func PrepareQuery(db *sql.DB, path string) (*sql.Stmt, error) {
+	query, err := ReadQueryFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	return stmt, nil
 }
